24a7a8972e3c266dd88905e701b43371f45443b4
Alexander Ferguson <alexander@buuteeq.com>
2014-07-30 12:32:06 -0700

    Add more comments to code for David

--- animateMenu.js
+++ animateMenu.js
@@ -23,7 +23,7 @@
                 $(this).hide();
                 z++;
             });
-            menuContainer.find('#menu-language-toggle-button').css({
+            menuContainer.find('#menu-language-toggle-button').css({ // Using this id everywhere could get messy. Could you use a variable instead? Is there a way to set the id value as an option on calling animateMenu() ?
                 width: 2.5 + 'em',
                 height: 2.5 + 'em',
                 zIndex: z,
@@ -34,10 +34,10 @@
 
         var lineAnimate = function(){
             if(menuContainer.hasClass('closed')){
-                $('#menu-language-toggle-button').unbind('click');
+                $('#menu-language-toggle-button').unbind('click'); //what are some potential issues with unbinding click? I'd look in to event namespaces
                 var gap = 0;
                 var delay = 0;
-                menuContainer.find('li').each(function() {
+                menuContainer.find('li').each(function() { // Is there any way you could abstract these slideIn/slideOut functions and merely pass them variables?
                     $(this).delay(delay).show().animate({
                         marginLeft: gap + 'em'
                     }, speed);
@@ -46,16 +46,16 @@
                 });
                 menuContainer.find('li').promise().done(function(){
                     $('#menu-language-toggle-button').on("click", function(){
-                        lineAnimate();
+                        lineAnimate(); // Is this inherently bound to an object? What potential issues could arise if there are multiple items using animateMenu? how could you fix that?
                     });
                 });
-                menuContainer.removeClass('closed');
+                menuContainer.removeClass('closed'); // These can be chained together foo.removeClass('bar').addClass('bar1')
                 menuContainer.addClass('open');
             } else if(menuContainer.hasClass('open')){
                 $('#menu-language-toggle-button').unbind('click');
                 var gap = 0;
                 var delay = speed * menuLength;
-                menuContainer.find('li').each(function() {
+                menuContainer.find('li').each(function() { // Is there shared code between this and the slideOut function that you could re-use/abstract some way to compress the code?
                      $(this).delay(delay).animate({
                             marginLeft: startX
                         }, speed, function(){
