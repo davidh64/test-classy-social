1d530f015675cc56605871701eae6aa60e83dda0
davidh64 <davidh64@msn.com>
2014-07-30 12:34:36 -0700

    add comments

--- animateMenu.js
+++ animateMenu.js
@@ -1,47 +1,49 @@
 (function($) {
     $.fn.animateMenu = function(){
-        var menuContainer = $(this);
-        var startX = menuContainer.find('li :first-child').css('marginLeft');
-        var startY = menuContainer.find('li :first-child').css('marginTop');
-        var speed = 400;
-        var menuLength = menuContainer.find('li').length;
-        var spacing = 4;
 
+        var menuContainer = $(this); // object for the list of links
+        var startX = menuContainer.find('li :first-child').css('marginLeft'); // position of first list item
+        var speed = 400; // time the animation lasts in milliseconds
+        var menuLength = menuContainer.find('li').length; // length of the list
+        var spacing = 4; // space between buttons after the animation is complete
+
+        /* adds an item to the beginning of the list for the button that will expand and collapse the menu */
+        /* arranges z-indices of list items to appear properly during animations */
+        /* hides all items except the toggle button */
         var createMenu = function(){
             var z = 0;
-            menuContainer.prepend('<li id="menu-language-toggle-button" style="display: inline-block; width: 2.5em; height: 2.5em;"></li>');
+            menuContainer.prepend('<li id="menu-language-toggle-button" style="color: blue;"></li>'); // creates the toggle button
             menuContainer.find('li').each(function() {
                 $(this).css({
                     display: 'inline-block',
-                    width: 2.5 + 'em',
+                    width: 2.5 + 'em', 
                     height: 2.5 + 'em',
                     lineHeight: 2.5 + 'em',
                     textAlign: 'center',
                     zIndex: z,
-                    position: 'absolute'
+                    position: 'absolute',
+                    float: 'left'
                 });
                 $(this).hide();
                 z++;
             });
             menuContainer.find('#menu-language-toggle-button').css({
-                width: 2.5 + 'em',
-                height: 2.5 + 'em',
-                zIndex: z,
-                position: 'absolute'
+                zIndex: z
             });
             $('#menu-language-toggle-button').show();
         }
 
+        /* function to expand or collapse the menu whenever the toggle button is clicked */
         var lineAnimate = function(){
-            if(menuContainer.hasClass('closed')){
+            if(menuContainer.hasClass('closed')){ // the list is not expanded
                 $('#menu-language-toggle-button').unbind('click');
-                var gap = 0;
+                var newXPosition = 0;
                 var delay = 0;
                 menuContainer.find('li').each(function() {
                     $(this).delay(delay).show().animate({
-                        marginLeft: gap + 'em'
+                        left: newXPosition + 'em'
                     }, speed);
-                    gap += spacing;
+                    newXPosition += spacing;
                     delay += speed - (0.4 * speed);
                 });
                 menuContainer.find('li').promise().done(function(){
@@ -51,19 +53,19 @@
                 });
                 menuContainer.removeClass('closed');
                 menuContainer.addClass('open');
-            } else if(menuContainer.hasClass('open')){
+            } else if(menuContainer.hasClass('open')){ // the list is expanded
                 $('#menu-language-toggle-button').unbind('click');
-                var gap = 0;
+                var newXPosition = 0;
                 var delay = speed * menuLength;
                 menuContainer.find('li').each(function() {
                      $(this).delay(delay).animate({
-                            marginLeft: startX
+                            left: startX
                         }, speed, function(){
                             if(!$(this).is('#menu-language-toggle-button')){
                                $(this).hide();
                             }
                     });
-                    gap += spacing;
+                    newXPosition += spacing;
                     delay -= speed - (0.4 * speed);
                 });
                 menuContainer.find('li').promise().done(function(){
@@ -73,7 +75,7 @@
                 });
                 menuContainer.removeClass('open');
                 menuContainer.addClass('closed');
-            } else {
+            } else { // the list is being opened for the first time
                 menuContainer.addClass('closed');
                 lineAnimate();
             }
