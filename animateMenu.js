8b31787bb75e995f3e8dbc515fd5bbfeba94e2ac
davidh64 <davidh64@msn.com>
2014-07-30 17:27:42 -0700

    fix some issues according to Alexander's suggestions

--- animateMenu.js
+++ animateMenu.js
@@ -2,7 +2,8 @@
     $.fn.animateMenu = function(){
 
         var menuContainer = $(this); // object for the list of links
-        var startX = menuContainer.find('li :first-child').css('marginLeft'); // position of first list item
+        var toggleButton;
+        var startX; // position of first list item
         var speed = 400; // time the animation lasts in milliseconds
         var menuLength = menuContainer.find('li').length; // length of the list
         var spacing = 4; // space between buttons after the animation is complete
@@ -12,78 +13,85 @@
         /* hides all items except the toggle button */
         var createMenu = function(){
             var z = 0;
-            menuContainer.prepend('<li id="menu-language-toggle-button" style="color: blue;"></li>'); // creates the toggle button
+            menuContainer.prepend('<li id="menu-toggle-button" style="color: blue;"></li>'); // creates the toggle button
+            toggleButton = $('#menu-toggle-button');
             menuContainer.find('li').each(function() {
                 $(this).css({
                     display: 'inline-block',
-                    width: 2.5 + 'em', // Javascript does dynamic typing, so you can write these out as strings '2.5em'
-                    height: 2.5 + 'em',
-                    lineHeight: 2.5 + 'em',
+                    width: '2.5em',
+                    height: '2.5em',
+                    lineHeight: '2.5em',
                     textAlign: 'center',
                     zIndex: z,
-                    position: 'absolute',
-                    float: 'left'
+                    position: 'absolute'
                 });
                 $(this).hide();
                 z++;
             });
-            menuContainer.find('#menu-language-toggle-button').css({ // Using this id everywhere could get messy. Could you use a variable instead? Is there a way to set the id value as an option on calling animateMenu() ?
+            toggleButton.css({ // Using this id everywhere could get messy. Could you use a variable instead? Is there a way to set the id value as an option on calling animateMenu() ?
                 zIndex: z
             });
-            $('#menu-language-toggle-button').show();
+            startX = toggleButton.position().left;
+            toggleButton.show();
         }
 
         /* function to expand or collapse the menu whenever the toggle button is clicked */
         var lineAnimate = function(){
             if(menuContainer.hasClass('closed')){ // the list is not expanded
-                $('#menu-language-toggle-button').unbind('click'); //what are some potential issues with unbinding click? I'd look in to event namespaces
-                var newXPosition = 0;
+                toggleButton.unbind('click', lineAnimate); //what are some potential issues with unbinding click? I'd look in to event namespaces
+                var newPosition = 0;
                 var delay = 0;
                 menuContainer.find('li').each(function() { // Is there any way you could abstract these slideIn/slideOut functions and merely pass them variables?
                     $(this).delay(delay).show().animate({
-                        left: newXPosition + 'em'
+                        left: newPosition + 'em'
                     }, speed);
-                    newXPosition += spacing;
+                    //slide(this, delay, newPosition, false);
+                    newPosition += spacing;
                     delay += speed - (0.4 * speed);
                 });
                 menuContainer.find('li').promise().done(function(){
-                    $('#menu-language-toggle-button').on("click", function(){
-                        lineAnimate(); // Is this inherently bound to an object? What potential issues could arise if there are multiple items using animateMenu? how could you fix that?
-                    });
+                    toggleButton.bind('click', lineAnimate);
+                    // Is this inherently bound to an object? What potential issues could arise if there are multiple items using animateMenu? how could you fix that?
                 });
-                menuContainer.removeClass('closed'); // These can be chained together foo.removeClass('bar').addClass('bar1')
-                menuContainer.addClass('open');
+                menuContainer.removeClass('closed').addClass('open');
             } else if(menuContainer.hasClass('open')){ // the list is expanded
-                $('#menu-language-toggle-button').unbind('click');
-                var newXPosition = 0;
+                toggleButton.unbind('click', lineAnimate);
+                var newPosition = 0;
                 var delay = speed * menuLength;
                 menuContainer.find('li').each(function() { // Is there shared code between this and the slideOut function that you could re-use/abstract some way to compress the code?
                      $(this).delay(delay).animate({
                             left: startX
                         }, speed, function(){
-                            if(!$(this).is('#menu-language-toggle-button')){
+                            if(!$(this).is('#menu-toggle-button')){
                                $(this).hide();
                             }
                     });
-                    newXPosition += spacing;
+                    //slide(this, delay, startX, true);
+                    newPosition += spacing;
                     delay -= speed - (0.4 * speed);
                 });
                 menuContainer.find('li').promise().done(function(){
-                    $('#menu-language-toggle-button').on("click", function(){
-                        lineAnimate();
-                    });
+                    toggleButton.bind('click', lineAnimate);
                 });
-                menuContainer.removeClass('open');
-                menuContainer.addClass('closed');
+                menuContainer.removeClass('open').addClass('closed');
             } else { // the list is being opened for the first time
                 menuContainer.addClass('closed');
                 lineAnimate();
             }
         }
 
+        /* function to abstract animation code, not working yet */
+        // var slide = function(item, delay, endPosition, shouldHide){
+        //     $(this).delay(delay).animate({
+        //             left: endPosition
+        //         }, speed, function(){
+        //             if(shouldHide){
+        //                $(this).hide();
+        //             }
+        //     });
+        // }
+
         createMenu();
-        $('#menu-language-toggle-button').click(function (){
-            lineAnimate();
-        });
+        toggleButton.bind('click', lineAnimate);
     };
 })(jQuery);
