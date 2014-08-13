2f5666cf00bccbd9be93e16e26e57e50d165fdd1
davidh64 <davidh64@msn.com>
2014-08-13 16:57:06 -0700

    Fix positioning

--- animateMenu.js
+++ animateMenu.js
@@ -1,5 +1,5 @@
-(function($) {
-    $.fn.animateMenu = function(userOptions){
+(function ($) {
+    $.fn.animateMenu = function (userOptions) {
 
         var menuContainer = $(this); // object for the list of links
         var toggleButton;
@@ -13,26 +13,33 @@
             height: '2.5em',
             backgroundColor: 'blue',
             lineHeight: '2.5em',
-            textAlign: 'center'
+            textAlign: 'center',
+            buttonText: '',
+            buttonClass: []
         }
         $.extend(options, userOptions);
 
         /* adds an item to the beginning of the list for the button that will expand and collapse the menu */
         /* arranges z-indices of list items to appear properly during animations */
         /* hides all items except the toggle button */
-        var createMenu = function(){
+        var createMenu = function () {
             var z = 0;
             menuContainer.css({
-                listStyleType: 'none'
+                listStyleType: 'none',
+                position: 'absolute'
             });
             menuContainer.prepend('<li></li>'); // creates the toggle button
             toggleButton = menuContainer.find('li').first();
-            if(options.direction == 'right'){
+            toggleButton.html(options.buttonText);
+            for (var i = 0; i < options.buttonClass.length; i++) {
+                toggleButton.addClass(options.buttonClass[i]);
+            }
+            if (options.direction == 'right') {
                 startX = toggleButton.position().left + (menuContainer.find('li').length - 1) * options.spacing;
             } else {
                 startX = toggleButton.position().left;
             }
-            menuContainer.find('li').each(function() {
+            menuContainer.find('li').each(function () {
                 $(this).css({
                     display: 'inline-block',
                     width: options.width,
@@ -50,39 +57,39 @@
             toggleButton.css({
                 zIndex: z
             });
-            
             toggleButton.show();
         }
 
         /* function to expand or collapse the menu whenever the toggle button is clicked */
-        var lineAnimate = function(){
-            if(menuContainer.hasClass('open')){ // the list is expanded
+        var lineAnimate = function () {
+            startX = toggleButton.position().left;
+            if (menuContainer.hasClass('open')) { // the list is expanded
                 toggleButton.unbind('click', lineAnimate);
-                var delay = options.speed * menuLength - 1;
-                menuContainer.find('li').each(function() {
-                    if(!$(this).is(toggleButton)){
+                var delay = options.speed * menuLength - 2;
+                menuContainer.find('li').each(function () {
+                    if (!$(this).is(toggleButton)) {
                         slide($(this), delay, startX, true);
                     }
                     delay -= options.speed - (0.4 * options.speed);
                 });
-                menuContainer.find('li').promise().done(function(){
+                menuContainer.find('li').promise().done(function () {
                     toggleButton.bind('click', lineAnimate);
                 });
                 menuContainer.removeClass('open');
             } else { // the list is not expanded
                 toggleButton.unbind('click', lineAnimate);
                 var newPosition = startX;
-                var delay = options.speed;
-                menuContainer.find('li').each(function() {
+                var delay = 0;
+                menuContainer.find('li').each(function () {
                     slide($(this), delay, newPosition, false);
-                    if(options.direction == 'right'){
+                    if (options.direction == 'right') {
                         newPosition -= options.spacing;
                     } else {
                         newPosition += options.spacing;
                     }
                     delay += options.speed - (0.4 * options.speed);
                 });
-                menuContainer.find('li').promise().done(function(){
+                menuContainer.find('li').promise().done(function () {
                     toggleButton.bind('click', lineAnimate);
                 });
                 menuContainer.addClass('open');
@@ -90,13 +97,13 @@
         }
 
         /* function to abstract animation code */
-        var slide = function(item, delay, endPosition, shouldHide){
+        var slide = function (item, delay, endPosition, shouldHide) {
             item.delay(delay).show().animate({
-                    left: endPosition
-                }, options.speed, function(){
-                    if(shouldHide){
-                       $(this).hide();
-                    }
+                left: endPosition
+            }, options.speed, 'linear', function () {
+                if (shouldHide) {
+                    $(this).hide();
+                }
             });
         }
 
